0c401a2a7042018376308592dfe8353fb6ce1df1
jeremiasleite <jeremias23456@gmail.com>
2020-02-04 11:35:54 -0300

    projeto criado

---
+++ src/main/java/com/jeremiasleite/ocorretor/OcorretorApplication.java
@@ -0,0 +1,13 @@
+package com.jeremiasleite.ocorretor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcorretorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcorretorApplication.class, args);
+	}
+
+}
