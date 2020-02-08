85c662f856b28e57f9248e47d6b68d8365853f62
jeremiasleite <jeremias23456@gmail.com>
2020-02-08 13:05:35 -0300

    Modificação das anotações string

--- src/main/java/com/jeremiasleite/ocorretor/resources/ImovelResource.java
+++ src/main/java/com/jeremiasleite/ocorretor/resources/ImovelResource.java
@@ -2,6 +2,7 @@ package com.jeremiasleite.ocorretor.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,17 +18,17 @@ public class ImovelResource {
 	@Autowired
 	private ImovelService service;
 	
-	@RequestMapping(value="/ola", method = RequestMethod.GET)
+	@GetMapping(value="/ola")
 	public String Ola() {
 		return "oi";
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<?> findAll(){
 		return ResponseEntity.ok().body(service.findAll());
 	}
 	
-	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
+	@GetMapping(value= "/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Imovel imovel = service.find(id);		
